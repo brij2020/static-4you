8f71a12cf24f0110e28fa11eadd0f00a6ceedf52
Brijbhna Chauhan <brijphy@gmail.com>
2022-11-19 22:05:26 +0530

    adding cookie test

--- assets/js/api/paymentPaytm.js
+++ assets/js/api/paymentPaytm.js
@@ -24,6 +24,9 @@ const btn = document.getElementById('pay-btn');
               url: 'https://ecom-electronic.onrender.com/payment/book-slot',
               cors: 'no-cors',
               data: serverData,
+               xhrFields: {
+                withCredentials: true
+                },
               success: function({data:paymentResponse}) { 
                 
                   console.log(paymentResponse,'paymentResponse')
