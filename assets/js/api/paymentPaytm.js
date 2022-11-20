0060946d982824df7f284aa436c3724121600975
Brijbhna Chauhan <brijphy@gmail.com>
2022-11-20 16:56:13 +0530

    loader implementedd

--- assets/js/api/paymentPaytm.js
+++ assets/js/api/paymentPaytm.js
@@ -1,81 +1,94 @@
 const btn = document.getElementById('pay-btn');
-      btn.addEventListener('click', async function(e) {
-          e.preventDefault()
-          const name  = document.getElementById('name').value;
-          const lastName  = document.getElementById('last-name').value;
-          const email  = document.getElementById('email-id').value;
-          const mobile  = document.getElementById('ph-number').value;
-          const gst  = document.getElementById('gst').value;
-         
+const loader = document.getElementsByClassName('loader')[0];
 
-       const serverData = { 
-        name: name, 
-        email: email,
-        gst: gst, 
-        mobile: mobile,  
-        "itemsPrice":"423.00",
-        "shippingPrice":"0.00",
-        "taxPrice":"64.53",
-        "totalPrice":"487.53",
-        "fallBackUrl": "https://static-4you.vercel.app",
-        "merchantUrl": "https://securegw-stage.paytm.in" }
-      var saveData = $.ajax({
-              type: 'POST',
-              url: 'https://ecom-electronic.onrender.com/payment/book-slot',
-              cors: 'no-cors',
-              data: serverData,
-              success: function({data:paymentResponse}) { 
-                
-                  console.log(paymentResponse,'paymentResponse')
-                // Start 
-                if(paymentResponse) {
-                    const scriptTag = document.createElement('script');
-                    scriptTag.src = paymentResponse.paymentData.orderData.paytm_marchent_url;
-                    scriptTag.type = "application/javascript"
-                    scriptTag.async = true;
-                    document.body.appendChild(scriptTag);
-                        scriptTag.onload = () => {
-                            console.log('testhhh')
-                            var config = {
-                                    "root": "",
-                                    "flow": "DEFAULT",
-                                    "data": {
-                                    "orderId": paymentResponse.paymentData.orderData.orderId, /* update order id */
-                                    "token": paymentResponse.paymentData.tokenInfo.txnToken, /* update token value */
-                                    "tokenType": "TXN_TOKEN",
-                                    "amount": {
-                                        ...paymentResponse.paymentData.orderData.txnAmount
-                                    }
-                                    
-                                },
-                                "handler": {
-                                    "notifyMerchant": function(eventName,data){
-                                    console.log("notifyMerchant handler function called");
-                                    console.log("eventName => ",eventName);
-                                    console.log("data => ",data);
-                                    }
+btn.addEventListener('click', async function(e) {
+    e.preventDefault()
+    const name  = document.getElementById('name').value;
+    const lastName  = document.getElementById('last-name').value;
+    const email  = document.getElementById('email-id').value;
+    const mobile  = document.getElementById('ph-number').value;
+    const gst  = document.getElementById('gst').value;
+    btn.style.display = 'none';
+    loader.style.display = 'block';
+    const serverData = { 
+    name: name, 
+    email: email,
+    gst: gst, 
+    mobile: mobile,  
+    "itemsPrice":"423.00",
+    "shippingPrice":"0.00",
+    "taxPrice":"64.53",
+    "totalPrice":"487.53",
+    "fallBackUrl": "https://static-4you.vercel.app",
+    "merchantUrl": "https://securegw-stage.paytm.in" }
+    var saveData = $.ajax({
+            type: 'POST',
+            url: 'https://ecom-electronic.onrender.com/payment/book-slot',
+            cors: 'no-cors',
+            data: serverData,
+            success: function({data:paymentResponse}) { 
+            
+            loader.style.display = 'none';
+            // Start 
+            if(paymentResponse) {
+                const scriptTag = document.createElement('script');
+                scriptTag.src = paymentResponse.paymentData.orderData.paytm_marchent_url;
+                scriptTag.type = "application/javascript"
+                scriptTag.async = true;
+                document.body.appendChild(scriptTag);
+                    scriptTag.onload = () => {
+                        console.log('testhhh')
+                        var config = {
+                                "root": "",
+                                "flow": "DEFAULT",
+                                "data": {
+                                "orderId": paymentResponse.paymentData.orderData.orderId, /* update order id */
+                                "token": paymentResponse.paymentData.tokenInfo.txnToken, /* update token value */
+                                "tokenType": "TXN_TOKEN",
+                                "amount": {
+                                    ...paymentResponse.paymentData.orderData.txnAmount
                                 }
-                                };
-                                if(window.Paytm && window.Paytm.CheckoutJS){
-                                    window.Paytm.CheckoutJS.onLoad(function excecuteAfterCompleteLoad() {
-                                    // initialze configuration using init method
-                                    window.Paytm.CheckoutJS.init(config).then(function onSuccess() {
-                                    // after successfully updating configuration, invoke JS Checkout
-                                    window.Paytm.CheckoutJS.invoke();
-                                    }).catch(function onError(error){
-                                        console.log("error => ",error);
-                                    });
-                                    });
+                                
+                            },
+                            "handler": {
+                                "notifyMerchant": function(eventName,data){
+                                console.log("notifyMerchant handler function called");
+                                console.log("eventName => ",eventName);
+                                console.log("data => ",data);
                                 }
-                        }
-                
-                }
-
-        // End
+                            }
+                            };
+                            if(window.Paytm && window.Paytm.CheckoutJS){
+                                window.Paytm.CheckoutJS.onLoad(function excecuteAfterCompleteLoad() {
+                                // initialze configuration using init method
+                                window.Paytm.CheckoutJS.init(config).then(function onSuccess() {
+                                // after successfully updating configuration, invoke JS Checkout
+                                window.Paytm.CheckoutJS.invoke();
+                                }).catch(function onError(error){
+                                    console.log("error => ",error);
+                                });
+                                });
+                            }
+                    }
+            
             }
-        });
-      saveData.error(function() { alert("Something went wrong"); });
-         
 
+    // End
+        }
+    });
+    saveData.error(function() { alert("Something went wrong"); loader.style.display = 'none'; });
+})
 
-      })
+const closeBtn = document.getElementsByClassName('btn-close')[0];
+closeBtn.addEventListener('click', () => {
+
+    const btn = document.getElementById('pay-btn');
+    btn.style.display = 'block'
+})
+
+const serviceBtn = document.getElementsByClassName('btn-main-service')[0];
+serviceBtn.addEventListener('click', () => {
+    const btn = document.getElementById('pay-btn');
+    btn.style.display = 'block'
+})
+
